dc7f22c5597a9f3cbf87526a99b61f7ad2f95d59
DoiMayank <doi.mayank@gmail.com>
2026-05-02 11:47:49 +0530

    TBB

---
+++ TBB/TBB/Program.cs
@@ -0,0 +1,34 @@
+﻿using Turn_Based_Arcade_Game;
+
+Unit player = new Unit(100,20,12,"Player");
+
+
+Unit enemy = new Unit(75,10,10,"Enemy Mage");
+Random random = new Random();
+
+while (!player.IsDead && !enemy.IsDead)
+{
+    Console.WriteLine(player.UnitName + "HP = " + player.Hp + ". " + enemy.UnitName + "HP = " + enemy.Hp);
+
+
+    Console.WriteLine("Player turn ! What will you do ?");
+    string choice = Console.ReadLine();
+
+    if (choice == "a")
+        player.Attack(enemy);
+    else
+        player.Heal();
+    
+    if(player.IsDead || enemy.IsDead)break;
+
+    Console.WriteLine(player.UnitName + "HP = " + player.Hp + ". " + enemy.UnitName + "HP = " + enemy.Hp);
+
+    Console.WriteLine("Enemy turn !");
+
+    int rand = random.Next(0, 2);
+
+    if (rand == 0)
+        enemy.Attack(player);
+    else enemy.Heal();
+
+}
